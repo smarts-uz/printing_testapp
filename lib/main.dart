f5c2e81d1b5005d623a88de11c1819f75d8aafef
JasurbekErgashev <jergashev633@gmail.com>
2023-10-11 16:55:56 +0500

    added platform specific installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 void main() {
   runApp(const MyApp());
